947fd3416b6570d6e664d4a5a8ad236d3f3584f2
pjohalloran <pjohalloran@gmail.com>
2013-07-08 22:46:21 +0100

    Public interface for maxrects algorithm.

--- src/packing_algorithms/maxrects/texture_packer_maxrects.py
+++ src/packing_algorithms/maxrects/texture_packer_maxrects.py
@@ -1,8 +1,25 @@
 from packing_algorithms.texture_packer import TexturePacker
 
 
+class FreeRectChoiceHeuristicEnum:
+    (RectBestShortSideFit, RectBestLongSideFit, RectBestAreaFit, RectBottomLeftRule, RectContactPointRule) = range(0, 5)
+
+
 class TexturePackerMaxRects(TexturePacker):
-    texArr = None
+    usedRectList = None
+    freeRectList = None
+    binWidth = 0
+    binHeight = 0
+
+    def __init__(self, width=0, height=0):
+        TexturePacker.__init__(self)
+        self.usedRectList = []
+        self.freeRectList = []
+        self.binWidth = width
+        self.binHeight = height
+
+    def get_occupancy(self):
+        raise NotImplementedError('get_occupancy() has not been implemented')
 
     def pack_textures(self, powerOfTwo, oneBorderPixel):
         raise NotImplementedError('pack_textures() has not been implemented')
